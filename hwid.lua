e31d3aea6ec6d05af1bf92799826874cf347146b
heqds <93957669+heqds@users.noreply.github.com>
2022-12-20 13:22:45 -0500

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,4 +1,5 @@
 local hint = Instance.new("Hint",game.CoreGui)
 hint.Text = "Successfully copied your HWID to your clipboard."
 delay(5,function() hint:destroy() end)
-setclipboard(game:GetService("RbxAnalyticsService"):GetClientId())
+cboard = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set)
+cboard(game:GetService("RbxAnalyticsService"):GetClientId())
